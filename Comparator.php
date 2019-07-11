8ab19d43f3faeec1118b0bcfa32749134669903f
Simon <Gtingwen@outlook.com>
2019-07-11 15:29:03 -0400

    document preview added; file listing order changed.

--- Comparator.php
+++ Comparator.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+    <link rel="stylesheet" href="styles.css" type="text/css" charset="utf-8"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <meta charset="UTF-8">
     <title>File Comparison</title>
@@ -19,7 +21,13 @@ function print_files($mirrorfoldername){
             while (($file = readdir($dh)) !== false) {
                 if($file != "." && $file != "..") {
                     $filepath = $mirrorfoldername."/".$file;
-                    echo "<p>".$file." ".date("F d Y H:i:s.", filemtime($filepath))."</p>";
+                    $date = date("F/d/Y/H:i:s", filemtime($filepath));
+                    $size = filesize($filepath).' bytes';
+                    echo "<div class='row' style='padding-bottom: 0.5em'>";
+                    echo "<div class='col-4'>$file</div>
+                    <div class='col-2'>$size</div>
+                    <div class='col-3' style='text-align: left'>$date</div>";
+                    echo '</div>';
                     array_push($mirror_list, $file);
                 }
             }
@@ -31,6 +39,7 @@ function print_files($mirrorfoldername){
 function find_difference($mirrorfoldername, $livefoldername){
     $mirror_list = array();
     $live_list = array();
+
     if (is_dir($mirrorfoldername)){
         if ($dh = opendir($mirrorfoldername)) {
             while (($file = readdir($dh)) !== false) {
@@ -42,39 +51,105 @@ function find_difference($mirrorfoldername, $livefoldername){
             closedir($dh);
         }
     }
+
+    $new_mirrorFile_list = array();
+
     foreach ($mirror_list as $file){
         $filepath = $livefoldername."/".$file;
+        $mirrorpath = $mirrorfoldername.'/'.$file;
+        // Files that are new
         if(!file_exists($filepath)) {
-            echo "<div style='margin-bottom: 0.6em'>$file&emsp;NEW&emsp;<button onclick=\"transferDoc("."'$file'".")\" class='btn btn-primary btn-sm'>Transfer</button></div>";
+                try {
+                    $date = date("F/d/Y/H:i:s", filemtime($mirrorpath));
+                }catch (Exception $e){
+                    $date = 'N/A';
+                }
+                try {
+                    $size = filesize($mirrorpath).' bytes';
+                }catch (Exception $e){
+                    $size = 'N/A';
+                }
+                array_push($new_mirrorFile_list, $file);
+
+            /* echo "<div class=\"row\">";
+            echo "<div class=\"col-2\" style='margin-bottom: 0.6em'>$file&emsp;</div>
+            <div class=\"col-2\">NEW</div>
+            <div class=\"col-2\">$size</div>
+            <div class=\"col-2\">$date</div>
+            <div class=\"col-4\" style='text-align: right'><button onclick=\"transferDoc("."'$file'".")\" class='btn btn-primary btn-sm'>Transfer</button></div>";
+            echo "</div>"; */
         }elseif (filemtime($filepath)!=filemtime($mirrorfoldername."/".$file)){
-            echo "<div style='margin-bottom: 0.6em'>$file&emsp;UPDATED&emsp;<button onclick=\"transferDoc("."'$file'".")\" class=\"btn btn-primary btn-sm\">Transfer</button>&emsp;<button onclick=\"displayDoc("."'$file'".")\" class=\"btn btn-primary btn-sm\">Compare</button></div>"; # TODO: Add in displayDoc() parameter.
+            try {
+                $date = date("F/d/Y/H:i:s", filemtime($mirrorpath));
+            }catch (Exception $e){
+                $date = 'N/A';
+            }
+            try {
+                $size = filesize($mirrorpath).' bytes';
+            }catch (Exception $e){
+                $size = 'N/A';
+            }
+            echo "<div class=\"row\">";
+            echo "<div class=\"col-2\" style='margin-bottom: 0.6em'>$file</div>
+            <div class=\"col-2\">UPDATED</div>
+            <div class=\"col-2\">$size</div>
+            <div class=\"col-2\">$date</div>
+            <div class=\"col-4\" style='text-align: right'><button onclick=\"transferDoc("."'$file'".")\" class=\"btn btn-primary btn-sm\">Transfer</button>&emsp;<button onclick=\"displayDoc("."'$file'".")\" class=\"btn btn-primary btn-sm\">Compare</button></div>";
+            echo "</div>";
         }
+    }
+
+    foreach ($new_mirrorFile_list as $file){
+        $mirrorpath = $mirrorfoldername.'/'.$file;
+        try {
+            $date = date("F/d/Y/H:i:s", filemtime($mirrorpath));
+        }catch (Exception $e){
+            $date = 'N/A';
+        }
+        try {
+            $size = filesize($mirrorpath).' bytes';
+        }catch (Exception $e){
+            $size = 'N/A';
+        }
+
+        echo "<div class=\"row\">";
+        echo "<div class=\"col-2\" style='margin-bottom: 0.6em'>$file&emsp;</div>
+        <div class=\"col-2\">NEW</div>
+        <div class=\"col-2\">$size</div>
+        <div class=\"col-2\">$date</div>
+        <div class=\"col-4\" style='text-align: right'><button onclick=\"transferDoc("."'$file'".")\" class='btn btn-primary btn-sm'>Transfer</button></div>";
+        echo "</div>";
     }
 }
 ?>
 
-<div style="background-color: lightgray; width: 65%; margin: auto; margin-bottom: 1.6em; margin-top: 2em;height: 3em" class="row rounded shadow">
+<div style="background-color: lightgray; width: 90%; margin: auto; margin-bottom: 1.6em; margin-top: 2em;height: 3em" class="row rounded shadow">
     <div class="col" style="text-align: center; font-size: 1.5em; vertical-align: center">Mirror</div>
     <div class="col" style="text-align: center; font-size: 1.5em">Live</div>
 </div>
 
-<div id="documents" class="row shadow p-3 mb-5 bg-white rounded" style="text-align: left; margin-bottom:1.5em;width: 65%; margin: auto">
-        <div class="col" name="mirror" style="border-style: solid; border-color: lightgray">
-        <?php
-            find_difference("New","Old");
-        ?>
+<div id="documents" class="row shadow p-3 mb-5 bg-white rounded container-fluid" style="text-align: left; margin-bottom:1.5em;width: 96%; margin: auto">
+        <div class="col-6" name="mirror" style="border-style: solid; border-color: lightgray">
+
+            <?php
+            //print_files("Old");
+            ?>
+            <?php
+                find_difference("New","Old");
+            ?>
+
         </div>
-        <div class="col" name="live" style="border-style: solid; border-color: lightgray">
+        <div class="col-6" name="live" style="border-style: solid; border-color: lightgray">
             <?php
                 print_files("Old");
             ?>
         </div>
 </div>
 
-<div id="display" class="row shadow p-3 mb-5 bg-white rounded" style="margin: auto;width: 70%; display: none">
-    <div class="col" id="mirrorfile" style="overflow-y: scroll; height: 10em;"></div>
-    <div class="col" id="livefile" style="overflow-y: scroll; height: 10em"></div>
+<div id="status" style="margin: auto;width: 90%; text-align: center; font-size: large">
 </div>
+
+<div id="table"></div>
 
 <script type="text/javascript">
     function transferDoc(file) {
@@ -90,10 +165,35 @@ function find_difference($mirrorfoldername, $livefoldername){
         }
 
     function displayDoc(file){
+        document.getElementById('status').innerHTML = 'Loading...';
+        $("#table").empty();
         var mirror_path = "./New/"+file, live_path = "./Old/"+file;
-        document.getElementById("display").style.display = ""; // TODO: Add display document function.
-        $("#livefile").load(mirror_path);
-        $("#mirrorfile").load(live_path);
+        var xmlhttp;
+        if (window.XMLHttpRequest)
+        {
+            // IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+            xmlhttp=new XMLHttpRequest();
+        }
+        else
+        {
+            // IE6, IE5 浏览器执行代码
+            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange=function()
+        {
+            if (xmlhttp.readyState==4 && xmlhttp.status==200)
+            {
+                var table = xmlhttp.responseText;
+                if(table == ''){
+                    document.getElementById('status').innerHTML = 'No difference between mirror and live';
+                }else {
+                    document.getElementById('status').innerHTML = ' ';
+                    $("#table").append(table);
+                }
+            }
+        }
+        xmlhttp.open("GET","displayDoc.php?LIVE=" + live_path + "&" + "MIRROR=" + mirror_path,true);
+        xmlhttp.send();
     }
 
 </script>
--- displayDoc.php
+++ displayDoc.php
@@ -1,3 +1,23 @@
 <?php
-$content = file_get_contents("./New/testfile.txt");
-echo $content;
+//echo "<link rel=\"stylesheet\" href=\"styles.css\" type=\"text/css\" charset=\"utf-8\"/>";
+$newFile = $_GET['MIRROR'];
+$liveFile = $_GET['LIVE'];
+require_once dirname(__FILE__).'../php-diff-master/lib/Diff.php';
+
+// Include two sample files for comparison
+$a = explode("\n", file_get_contents($newFile));
+$b = explode("\n", file_get_contents($liveFile));
+
+// Options for generating the diff
+$options = array(
+    //'ignoreWhitespace' => true,
+    //'ignoreCase' => true,
+);
+
+// Initialize the diff class
+$diff = new Diff($a, $b, $options);
+
+// Generate a side by side diff
+require_once dirname(__FILE__).'./php-diff-master/lib/Diff/Renderer/Html/SideBySide.php';
+$renderer = new Diff_Renderer_Html_SideBySide;
+echo $diff->Render($renderer);
